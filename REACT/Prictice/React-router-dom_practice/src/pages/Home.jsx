a04bef6e3fa64f112b0b7ccbd066b040ce29bf2a
Dilkhush Kumar <dk5847001@gmail.com>
2026-08-11 08:50:49 +0530

    react-dom practice

--- REACT/Prictice/React-router-dom_practice/src/pages/Home.jsx
+++ REACT/Prictice/React-router-dom_practice/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import "./Home.css";
 export default function Home(){
     return(
         <div className="Home">
-            <h1>hello, welcome to home page.</h1>
+            <h1>Hello🙋‍♀️, welcome to home page.</h1>
+            <div className="section1">
+                <div className="homeLeft">
+            <h2>Student Details</h2>
+            <button>Name : Dilkhush Kumar</button>
+            <button>Email : contact@internovatech.in</button>
+            <button>Mo. no. : +91 9905167559</button>
+            <button>Branch : B.Tech CSE</button>
+            <button>Prn : 240205131051</button>
+            <button>College : Sandip University</button>
+                </div>
+                {/* ------------------------------------- */}
+                <div className="homeRight">
+            <h2>Quick Action</h2>
+            <button>View your profile</button>
+            <button>Register for internship</button>
+            <button>Go to your enrollment page</button>
+            <button>View your progress</button>
+            <button>Got to certificate Deshboard</button>
+            <button>Got to Training page</button>
+                </div>
+            </div>
         </div>
     )
 }
