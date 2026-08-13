d1ef17dc25f945ed100adea5e4a51da123cf265c
Dilkhush Kumar <dk5847001@gmail.com>
2026-08-13 12:22:52 +0530

    Lottery2 completed

--- REACT/Lottery-2/src/components/LotteryGame.jsx
+++ REACT/Lottery-2/src/components/LotteryGame.jsx
@@ -1,7 +1,23 @@
-export default function LotteryGame(){
+import { useState } from "react";
+import "./Lottery.css";
+import {genTicket, sum} from "../helper/helper";
+import Ticket from "./Ticket";
+import Button from "./Button";
+
+export default function LotteryGame({n=3, winCondition}){
+    const[ticket, setTicket] = useState(genTicket(n))
+    let isWinning = winCondition(ticket);
+
+    let buyTicket = ()=>{
+        setTicket(genTicket(n));
+    }
     return(
         <div className="Lottry">
-            <h2>Lottery Game</h2>
+            <h2>Lottery Game</h2><br />
+            <Button action={buyTicket}/>
+            <Ticket ticket={ticket}/>
+            <p>sum of digits : {sum(ticket)}</p><br />
+            <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
     )
 }
--- REACT/Lottery-2/src/pages/Lottery.jsx
+++ REACT/Lottery-2/src/pages/Lottery.jsx
@@ -1,9 +1,15 @@
 import LotteryGame from "../components/LotteryGame"
+import TicketNum from "../components/TicketNum"
+import Ticket from "../components/Ticket"
+import { sum } from "../helper/helper"
 export default function Lottery(){
+    let winCondition = (ticket) =>{
+        return ticket[0] ===0;
+    }
     return(
         <div className="Lottery">
-            <h2>Welcome to Lottery Game</h2><br />
-            <LotteryGame />
+            <h2>Welcome to Lottery Game</h2><br />\
+            <LotteryGame n={4} winCondition={winCondition}/>
         </div>
     )
 }
