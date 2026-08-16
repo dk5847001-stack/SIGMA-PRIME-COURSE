b058ce6a0fbe7aa34fb85076bcf85bea11c31c4a
Dilkhush Kumar <dk5847001@gmail.com>
2026-08-16 23:07:39 +0530

    formik implemented

--- REACT/Prictice/Form/src/CommentsForm.jsx
+++ REACT/Prictice/Form/src/CommentsForm.jsx
@@ -1,56 +1,90 @@
 import { useState } from "react";
 import "./CommentsForm.css";
+import { Formik, Form, Field, useFormik } from 'formik';
+
+const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+        errors.username = "Username cannot be empty!"
+    }
+    return errors;
+};
 export default function CommentsForm() {
-    let [formData, setFormData] = useState(
-        {
-            username: "",
-            remarks: "write here comments...",
-            ratting: "5"
-        }
-    );
+    // let [formData, setFormData] = useState(
+    //     {
+    //         username: "",
+    //         remarks: "write here comments...",
+    //         ratting: "5"
+    //     }
+    // );
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            remarks: '',
+            ratting: '',
+        },
+        validate,
+        onSubmit: (values, { resetForm }) => {
+            setComment((prev) => [...prev, values]);
+
+            resetForm();
+        },
+    });
 
     let [comment, setComment] = useState([]);
 
-    let handleInputChange = (event) => {
-        setFormData((currData) => {
-            return { ...currData, [event.target.name]: event.target.value }
-        })
-    };
+    // let handleInputChange = (event) => {
+    //     setFormData((currData) => {
+    //         return { ...currData, [event.target.name]: event.target.value }
+    //     })
+    // };
 
-    let handleSubmitForm = (event) => {
-        event.preventDefault();
-        console.log(formData);
-        setComment((prevComment) => {
-            return [...prevComment, formData]
-        })
-        setFormData(
-            {
-                username: "",
-                remarks: "",
-                ratting: ""
-            }
-        )
-    }
+    // let handleSubmitForm = (event) => {
+    //     event.preventDefault();
+    //     console.log(formData);
+    //     setComment((prevComment) => {
+    //         return [...prevComment, formData]
+    //     })
+    //     setFormData(
+    //         {
+    //             username: "",
+    //             remarks: "",
+    //             ratting: ""
+    //         }
+    //     )
+    // }
     return (
         <div className="CommentsForm">
             <h2>Comments Form</h2><br />
-            <form onSubmit={handleSubmitForm}>
-                <input type="text" placeholder="username" value={formData.username} name="username" onChange={handleInputChange} /><br />
-                <textarea name="remarks" value={formData.remarks} onChange={handleInputChange}>comments</textarea><br />
-                <input type="number" placeholder="rating" min={1} max={5} value={formData.ratting} name="ratting" onChange={handleInputChange} /><br />
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="usernames">Username</label>
+                <input type="text" id="usernames" placeholder="username" value={formik.values.username} name="username" onChange={formik.handleChange} /><br />
+                {formik.errors.username ? (
+                    <p>{formik.errors.username}</p>
+                ) : null}
+                <label htmlFor="remark">Add Remarks</label>
+                <textarea
+                    name="remarks"
+                    id="remark"
+                    value={formik.values.remarks}
+                    onChange={formik.handleChange}
+                />
+                <label htmlFor="rattings">Ratting</label>
+                <input type="number" id="rattings" placeholder="rating" min={1} max={5} value={formik.values.ratting} name="ratting" onChange={formik.handleChange} /><br />
                 <button>Add Comment</button>
             </form><br /><br />
             {comment && (
                 <div className="comments">
                     <h3>All Comments</h3>
-                <div className="section2">
-                    {comment.map((comments, index) => (
-                        <div className="remarks" key={index}>
-                            <p>Username: {comments.username}</p>
-                            <p>Comments: {comments.remarks}</p>
-                            <p>Rating: {comments.ratting}</p>
-                        </div>
-                    ))}
+                    <div className="section2">
+                        {comment.map((comments, index) => (
+                            <div className="remarks" key={index}>
+                                <p>Username: {comments.username}</p>
+                                <p>Comments: {comments.remarks}</p>
+                                <p>Rating: {comments.ratting}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             )}
