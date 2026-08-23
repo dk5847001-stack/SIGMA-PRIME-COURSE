efa8258d94be58faea67c29621dd41e1bea13494
Dilkhush Kumar <dk5847001@gmail.com>
2026-08-23 13:04:21 +0530

    tailwind css added

---
+++ TAILWIND_CSS/BASIC/tailwind-basic/src/App.jsx
@@ -0,0 +1,14 @@
+import TAilwind from "./Tailwind";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-800 p-4">
+      <h1 className="text-yellow-500 text-center">
+        Hello Tailwind CSS 🚀
+      </h1><br /><hr className="text-blue-200" /><br />
+      <TAilwind />
+    </div>
+  );
+}
+
+export default App;
---
+++ TAILWIND_CSS/BASIC/tailwind-basic/src/Tailwind.jsx
@@ -0,0 +1,19 @@
+export default function Tailwind() {
+    return (
+       <div className="flex flex-col sm:flex-row flex-wrap gap-2">
+        <div className="box">This is First Div</div>
+        <div className="box">This is Second Div</div>
+        <div className="box">This is Third Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+        <div className="box">This is Forth Div</div>
+       </div>
+    );
+}
